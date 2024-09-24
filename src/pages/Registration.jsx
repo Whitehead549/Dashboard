99055242f3be6a2632e5733149d571ba6d8bb34d
Whitehead549 <finbareric549@gmail.com>
2024-09-24 23:13:45 +0100

    build first

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -10,7 +10,7 @@ const Registration = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-gray-800 via-gray-700 to-gray-800 p-4 rounded-lg shadow-md">
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-gray-800 via-gray-700 to-gray-800 p-4 ">
       <div className="bg-white shadow-lg rounded-lg p-2 max-w-md w-full mx-2">
         {showLogin ? (
           <>
@@ -29,7 +29,7 @@ const Registration = () => {
           <>
             <Signup />
             <div className="text-center pb-2">
-              <p className="text-gray-700 text-sm md:text-base">Already have an account?</p>
+              <p className="text-gray-700 text-sm md:text-base">Already have an Account?</p>
               <button
                 onClick={handleToggle}
                 className="text-blue-600 hover:underline font-semibold"
